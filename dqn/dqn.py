e42a5bb6b9c2187d7da19505e5752d38900794fd
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2020-04-27 12:54:34 -0700

    Refactor ReplayMemory

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -4,40 +4,7 @@ from tensorflow import keras
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import load_model
 
-
-class ReplayBuffer():
-    def __init__(self, max_size, input_dims):
-        self.mem_size = max_size
-        self.mem_cntr = 0
-
-        self.state_memory = np.zeros((self.mem_size, *input_dims),
-                                     dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, *input_dims),
-                                         dtype=np.float32)
-        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
-        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.int32)
-
-    def store_transition(self, state, action, reward, state_, done):
-        index = self.mem_cntr % self.mem_size
-        self.state_memory[index] = state
-        self.new_state_memory[index] = state_
-        self.reward_memory[index] = reward
-        self.action_memory[index] = action
-        self.terminal_memory[index] = 1 - int(done)
-        self.mem_cntr += 1
-
-    def sample_buffer(self, batch_size):
-        max_mem = min(self.mem_cntr, self.mem_size)
-        batch = np.random.choice(max_mem, batch_size, replace=False)
-
-        states = self.state_memory[batch]
-        states_ = self.new_state_memory[batch]
-        rewards = self.reward_memory[batch]
-        actions = self.action_memory[batch]
-        terminal = self.terminal_memory[batch]
-
-        return states, actions, rewards, states_, terminal
+from utils import ReplayMemory
 
 
 class Agent():
@@ -55,11 +22,11 @@ class Agent():
         self.batch_size = batch_size
         self.model_file = f'{model_dir}/{fname}'
         self.checkpoint_dir = ckpt_dir
-        self.memory = ReplayBuffer(mem_size, input_dims)
+        self.memory = ReplayMemory(mem_size, input_dims)
         self.q_eval = self._get_model(lr, n_actions, input_dims, 256, 256)
 
     def store_transition(self, state, action, reward, new_state, done):
-        self.memory.store_transition(state, action, reward, new_state, done)
+        self.memory.store(state, action, reward, new_state, done)
 
     def choose_action(self, observation):
         if np.random.random() < self.epsilon:
@@ -71,11 +38,11 @@ class Agent():
         return action
 
     def learn(self):
-        if self.memory.mem_cntr < self.batch_size:
+        if self.memory.current_size < self.batch_size:
             return
 
         states, actions, rewards, states_, dones = \
-            self.memory.sample_buffer(self.batch_size)
+            self.memory.sample(self.batch_size)
 
         q_eval = self.q_eval.predict(states)
         q_next = self.q_eval.predict(states_)
--- dqn/utils.py
+++ dqn/utils.py
@@ -5,6 +5,58 @@ import gym
 import tensorflow as tf
 
 
+class ReplayMemory():
+    def __init__(self, capacity, observation_shape):
+        self.capacity = capacity
+        self.dim = observation_shape
+        self.current_size = 0
+
+        self.states = np.zeros(
+            shape=(capacity, *observation_shape),
+            dtype=np.float32
+        )
+        self.actions = np.zeros(shape=capacity, dtype=np.int32)
+        self.rewards = np.zeros(shape=capacity, dtype=np.float32)
+        self.next_states = np.zeros(
+            shape=(capacity, *observation_shape),
+            dtype=np.float32
+        )
+        self.dones = np.zeros(shape=capacity, dtype=np.int32)
+
+    def store(self, state, action, reward, next_state, done):
+        """
+        Store the transition tuple <s, a, r, s', done> in the buffer
+        """
+        # Get the index to store, rotate the buffer
+        i = self.current_size % self.capacity
+
+        self.states[i] = state
+        self.actions[i] = action
+        self.rewards[i] = reward
+        self.next_states[i] = next_state
+        self.dones[i] = int(not done)
+
+        self.current_size += 1
+
+    def sample(self, size):
+        """
+        Sample random tuples of `size`
+        """
+        sample_indices = np.random.choice(
+            min(self.capacity, self.current_size),
+            size,
+            replace=False
+        )
+
+        return (
+            self.states[sample_indices],
+            self.actions[sample_indices],
+            self.rewards[sample_indices],
+            self.next_states[sample_indices],
+            self.dones[sample_indices]
+        )
+
+
 def write_summaries(summary_writer, values, step, env_name):
     descriptions = {
         'epsilon': 'Exploration probability',
